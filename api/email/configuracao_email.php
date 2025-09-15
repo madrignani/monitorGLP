d720065dcfe16f52c31fe2f9995fd18f30970553
Giovanni Madrignani <87835741+madrignani@users.noreply.github.com>
2025-09-15 17:07:37 -0300

    Update configuracao_email.php

--- api/email/configuracao_email.php
+++ api/email/configuracao_email.php
@@ -4,7 +4,7 @@
 define('SMTP_HOST', 'smtp.gmail.com');
 define('SMTP_PORT', 587);
 define('SMTP_USERNAME', 'monitorgasglp@gmail.com');
-define('SMTP_PASSWORD', 'qbon ange wxfv fiig');
+define('SMTP_PASSWORD', '');
 define('EMAIL_FROM', 'monitorgasglp@gmail.com');
 define('EMAIL_FROM_NAME', 'Monitor GLP');
 define('EMAIL_TO', 'giovannioliveira5@gmail.com');
